1f871b9c02caea6d35e2dc684a114fd3a30dc0df
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2024-02-21 11:34:42 +0000

    Remove ktlint functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,6 @@ plugins {
     kotlin("plugin.allopen")
 
     id("io.gatling.gradle") version "3.9.5.6"
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
-}
-
-ktlint {
-    disabledRules.set(setOf("no-wildcard-imports"))
 }
 
 gatling {
